13a50b6abad59ad1707a1318e40bf085292c7dfa
Chris Oden <coden@its.jnj.com>
2023-02-25 13:38:50 -0500

    Remove loading dotenv

--- sites/__dxp__/gatsby-config.js
+++ sites/__dxp__/gatsby-config.js
@@ -12,9 +12,9 @@ const { getDisabledPages } = require('@bodiless/components/node-api');
 
 const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `.env.${activeEnv}`,
-});
+// require('dotenv').config({
+//   path: `.env.${activeEnv}`,
+// });
 
 const SITEURL = process.env.SITE_URL;
 
