43439b332745f2f64dc33faf9d11d03695b07794
Chris Oden <coden@its.jnj.com>
2023-03-05 12:04:05 -0500

    Update postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 echo Postinstall on $(date) $(pwd)
-# ls node_modules/*
-# echo root node modules...
-# ls 'node_modules/*'
-# echo site node modules...
-# ls '@sites/__dxp__/node_modules/*'
-# npm run build:packages
 
 echo Building env vars...
-CURRENT=$(pwd)
-cd sites/__dxp__
-npm run build:env-vars
-cat .env.production | grep SITE_URL
-cd $CURRENT
+npm run build:env-vars --workspace @sites/--dxp--
 
 echo Building package...
-cd packages/__dxp__
-npm run build
-cd $CURRENT
+npm run build:packages
