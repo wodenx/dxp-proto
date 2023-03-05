699a750cddebc38025ca4b85929f3ca381ef211b
Chris Oden <coden@its.jnj.com>
2023-03-05 06:20:53 -0500

    Add postinstall, remove lerna

---
+++ postinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo Postinstall on $(date) $(pwd)
+# ls node_modules/*
+# echo root node modules...
+# ls 'node_modules/*'
+# echo site node modules...
+# ls '@sites/__dxp__/node_modules/*'
+# npm run build:packages
+
+echo Building env vars...
+CURRENT=$(pwd)
+cd sites/__dxp__
+npm run build:env-vars
+cat .env.production | grep SITE_URL
+cd $CURRENT
+
+echo Building package...
+cd packages/__dxp__
+npm run build
+// ls -l lib
+cd $CURRENT
