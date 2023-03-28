e9a2785d214ddc566931d4a9411209ec7905f530
Chris Oden <coden@its.jnj.com>
2023-03-28 05:29:09 -0400

    WIP

---
+++ packages/dxp-components/src/components/CopyrightRow/tokens/dxpCopyrightRow.ts
@@ -0,0 +1,52 @@
+import { vitalCopyrightRow, asCopyrightRowToken } from '@bodiless/vital-layout';
+import { as, on, replaceWith } from '@bodiless/fclasses';
+import { withSbContent, withSbContentFromParent } from '../../../util';
+import NodeTreePrinter, { withSbContentExample } from '../../../util/NodeTreePrinter';
+
+const Default = asCopyrightRowToken({
+  ...vitalCopyrightRow.Default,
+  Editors: {
+    Copyright: as(
+      replaceWith(EditorPlainClean),
+      dxpEditorPlain.Default,
+    ),
+  },
+  },
+  Content
+    Copyright: as(
+      vitalCopyrightRow.Default.Content,Copyright,
+      withSbContent(),
+    )
+  }
+});
+
+})
+
+export const Generic = asGenericTemplateToken({
+  ...vitalGenericTemplateBase.Generic,
+  Components: {
+    ...vitalGenericTemplateBase.Generic.Components,
+    TopContent: on(EditorPlainClean)(dxpEditorPlain.Default),
+    Content: on(NodeTreePrinter)(withSbContentExample),
+    BottomContent: on(EditorPlainClean)(dxpEditorPlain.StackbitContainerItem),
+  },
+  Theme: {
+    TopWrapper: vitalTypography.H1,
+  },
+  Content: {
+    // Content domin is "inside" Schema domain,m so these are invoked after
+    // The node-keys have been defined.  This way we avoid having to
+    // specify the node key in more thn one place.  Note that the node key
+    // must be the same as the stackbit field name.
+    TopContent: withSbContentFromParent(),
+    Content: withSbContentFromParent(),
+    BottomContent: withSbContentFromParent(),
+  },
+});
+
+const dxpGenericTemplate = {
+  ...vitalGenericTemplateBase,
+  Generic,
+};
+
+export default dxpGenericTemplate;
