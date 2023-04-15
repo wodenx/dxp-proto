ad8b21ed67f914c812e7a07ceac2cbd9eced3fae
Chris Oden <coden@its.jnj.com>
2023-04-15 16:08:23 -0400

    Fix sahdow of layout token

--- packages/__dxp__/src/shadow/@bodiless/vital-layout/Layout.ts
+++ packages/__dxp__/src/shadow/@bodiless/vital-layout/Layout.ts
@@ -6,7 +6,8 @@ import {
 } from '@bodiless/vital-layout';
 import HeaderClean from '../../../components/Layout/Header/HeaderClean';
 
-const Default = asLayoutToken(vitalLayoutBase.Default, {
+const Default = asLayoutToken({
+  ...vitalLayoutBase.Default,
   Components: {
     ...vitalLayoutBase.Default.Components,
     Header: on(HeaderClean)(vitalHeader.Default),
