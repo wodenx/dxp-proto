95f42622468196aeff02c0a3ace7e82233be8f7a
Samuel Davis <hello@smdvs.com>
2023-04-19 07:21:30 -0500

    Update footer styling

--- packages/__dxp__/src/components/Layout/Footer/tokens/listerineFooter.ts
+++ packages/__dxp__/src/components/Layout/Footer/tokens/listerineFooter.ts
@@ -1,57 +1,84 @@
 // TODO This file should move to Listerine package..
 
 import {
-  vitalFooterBase, asFooterToken, RewardsClean, vitalRewards
+  vitalFooterBase, asFooterToken, RewardsClean,
 } from '@bodiless/vital-layout';
 import {
+  // Div,
+  // P,
   addClasses,
-  addProps, as, flowHoc, on, withDesign
+  addProps, as, flowHoc, on, replaceWith, withDesign
 } from '@bodiless/fclasses';
 // import { listerineFontSize } from '../../../FontSize';
-import { vitalTextDecoration } from '@bodiless/vital-elements';
-import { listerineTypography } from '../../../Typography';
+// import { vitalTextDecoration } from '@bodiless/vital-elements';
+// import { withParent, withPrependChild } from '@bodiless/core';
+// import { listerineTypography } from '../../../Typography';\
+// import { listerineFontSize } from '../../../FontSize';
 import { listerineColor } from '../../../Color';
+import listerineRewards from '../../Rewards/tokens';
 
 const WithListerineRewardsExpanding2XL = asFooterToken({
-  ...vitalFooterBase.Default,
+  // ...vitalFooterBase.WithRewardsExpanding2XL,
   Components: {
-    Rewards: on(RewardsClean)(vitalRewards.Default),
+    Rewards: flowHoc(
+      on(RewardsClean)(listerineRewards.Default),
+      withDesign({
+        Footnote: replaceWith(() => null),
+      }),
+    ),
   },
   // @sam your choice if you want to use this token OR jsut rewrite this for listerine
   Theme: {
-    ...vitalFooterBase.Default.Theme,
+    // ...vitalFooterBase.WithRewardsExpanding2XL.Theme,
     _: listerineColor.TextWhite,
+    Wrapper: 'nothing',
     Column2Wrapper: flowHoc(
       as(listerineColor.BgSecondaryFooter),
-      addClasses('md:px-20'),
+      addClasses('md:pl-10 mx-2'),
     ),
-    Container: '2xl:m-0 2xl:pt-0 2xl:pb-0',
-    Rewards: flowHoc(
-      addClasses('2xl:py-20 2xl:px-10'),
-      withDesign({
-        Wrapper: as(listerineColor.BgSecondaryFooterSignUp),
-        Title: as(listerineTypography.H4),
-        Body: as(listerineTypography.Body),
-        FormWrapper: '2xl:flex 2xl:flex-row 2xl:flex-wrap',
-        FormTextFirstName: flowHoc(
-          addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
-          as(listerineTypography.Body),
-        ),
-        FormTextEmail: flowHoc(
-          addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
-          as(listerineTypography.Body),
-        ),
-        FormButton: flowHoc(
-          addClasses('2xl:bg-interactive-primary md:p-4 2xl:rounded'),
-          as(
-            listerineTypography.Body,
-            vitalTextDecoration.Uppercase,
-          ),
-        ),
-      })
-    ),
-    // @todo This is just to make it more visible, needs to be the correct token.
-  }
+    Container: '2xl:m-0 2xl:pt-0 2xl:pb-0 mx-0',
+    // Rewards: flowHoc(
+    //   addClasses('py-20 px-10'),
+    //   withDesign({
+    //     Wrapper: as(listerineColor.BgSecondaryFooterSignUp),
+    //     Title: as(listerineTypography.H4),
+    //     Body: as(listerineTypography.Body),
+    //     FormWrapper: flowHoc(
+    //       addClasses('2xl:flex 2xl:flex-row 2xl:flex-wrap'),
+    //     ),
+    //     FormTextFirstName: flowHoc(
+    //       addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
+    //       as(listerineTypography.Body),
+    //     ),
+    //     FormTextEmail: flowHoc(
+    //       addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
+    //       as(listerineTypography.Body),
+    //     ),
+    //     FormButton: flowHoc(
+    //       withParent(Div),
+    //       withDesign({
+    //         Parent: withPrependChild(
+    //           flowHoc(
+    //             addProps({
+    //               children:
+    //                  'The information you submit will be governed by our site’s PRIVACY POLICY'
+    //             }),
+    //             as(
+    //               listerineTypography.CrumbsReviews,
+    //               '2xl:text-white block 2xl:mb-3',
+    //             )
+    //           )(P), 'TEST'
+    //         ),
+    //       }),
+    //       addClasses('2xl:bg-interactive-primary md:p-4 2xl:rounded'),
+    //       as(
+    //         listerineTypography.Body,
+    //         vitalTextDecoration.Uppercase,
+    //       ),
+    //     ),
+    //   })
+    // ),
+  },
 });
 
 const Default = asFooterToken(vitalFooterBase.Default,
