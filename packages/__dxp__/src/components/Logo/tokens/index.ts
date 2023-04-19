133d24cee1e002b1967bbb22c7f01e0ed70c6411
Samuel Davis <hello@smdvs.com>
2023-04-19 07:19:12 -0500

    Add Listerine SVG Logo

--- packages/__dxp__/src/components/Logo/tokens/index.ts
+++ packages/__dxp__/src/components/Logo/tokens/index.ts
@@ -1,3 +1,6 @@
 import Logo from './listerineLogo';
+import ListerineLogo from '../assets/ListerineLogo';
+
+export { ListerineLogo };
 
 export default Logo;
--- packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
+++ packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
@@ -1,10 +1,13 @@
+import { replaceWith } from '@bodiless/fclasses';
 import { asLogoToken, vitalLogoBase } from '@bodiless/vital-layout';
+import ListerineLogo from '../assets/ListerineLogo';
 
 const Default = asLogoToken({
   ...vitalLogoBase.Default,
   Layout: {
     ...vitalLogoBase.Default.Layout,
     Wrapper: 'w-full max-w-logo md:max-w-28 lg:min-w-28',
+    Image: replaceWith(ListerineLogo),
   },
 });
 
