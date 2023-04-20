1c1f25af004766a3955fafd7416b6818ee7c57fb
Dewen Li <dewen.sinocrest@gmail.com>
2023-04-20 09:21:35 -0400

    feat(dxp-product): JEPZ-64 Product Category Section Mapping (Unstyled) (#194)
    * feat(dxp-product): JEPZ-64 Product Category Section Mapping (Unstyled)

--- packages/dxp-product/src/util/contentfulDataTransformer.ts
+++ packages/dxp-product/src/util/contentfulDataTransformer.ts
@@ -4,6 +4,7 @@ import type {
   DxpProductData,
   ContentfulProductData,
   ContentfulProductContent,
+  DxpProductCollectionCardData,
 } from '../components/ProductSection/types';
 
 class ProductDataTransformer implements DataTransformer<ContentfulProductData, DxpProductData> {
@@ -12,17 +13,46 @@ class ProductDataTransformer implements DataTransformer<ContentfulProductData, D
   }
 
   static transformProductCard(raw: ContentfulProductData): DxpProductCardData {
-    const productContent = ProductDataTransformer.parseContent(raw.content);
+    const {
+      contentful_id,
+      name,
+      images,
+    } = ProductDataTransformer.parseContent(raw.content);
     return {
-      id: productContent.contentful_id,
-      title: {text: productContent.name},
+      id: contentful_id,
+      title: {text: name},
       eyebrow: {text: ''},
       description: {text: ''},
       slug: raw.fields.slug,
       image: {
-        src: productContent.images[0].file.url,
-        title: productContent.images[0].title,
-        alt: productContent.images[0].description,
+        src: images[0].file.url,
+        title: images[0].title,
+        alt: images[0].description,
+      }
+    };
+  }
+
+  static transformProductCategory(raw: ContentfulProductData): DxpProductCollectionCardData {
+    const {
+      collection: {
+        title,
+        image,
+      },
+    } = ProductDataTransformer.parseContent(raw.content);
+    return {
+      // @todo: expect fetch-product script to provide collection id after.
+      id: title.replace(/\s+/g, '-').toLowerCase(),
+      title: {text: title},
+      eyebrow: {text: ''},
+      link: {
+        href: `/products/${raw.fields.slug}`,
+        'aria-label': `View all ${title} collections`,
+      },
+      slug: raw.fields.slug,
+      image: {
+        src: image?.fields.file.url,
+        title: image?.fields.title,
+        alt: image?.fields.title,
       }
     };
   }
