308b1fc09812413c4feb764b2abf724bc30261c4
Samuel Davis <hello@smdvs.com>
2023-04-20 11:30:47 -0500

    Style and refactor footer

--- packages/__dxp__/src/components/Layout/Footer/tokens/listerineFooter.ts
+++ packages/__dxp__/src/components/Layout/Footer/tokens/listerineFooter.ts
@@ -1,83 +1,37 @@
 // TODO This file should move to Listerine package..
 
 import {
-  vitalFooterBase, asFooterToken, RewardsClean,
+  vitalFooterBase, asFooterToken, RewardsClean
 } from '@bodiless/vital-layout';
 import {
-  // Div,
-  // P,
   addClasses,
-  addProps, as, flowHoc, on, replaceWith, withDesign
+  addProps, as, flowHoc, on, removeClasses, withDesign
 } from '@bodiless/fclasses';
-// import { listerineFontSize } from '../../../FontSize';
-// import { vitalTextDecoration } from '@bodiless/vital-elements';
-// import { withParent, withPrependChild } from '@bodiless/core';
-// import { listerineTypography } from '../../../Typography';\
-// import { listerineFontSize } from '../../../FontSize';
 import { listerineColor } from '../../../Color';
-import listerineRewards from '../../Rewards/tokens';
+import listerineRewards from '../Rewards/tokens';
 
 const WithListerineRewardsExpanding2XL = asFooterToken({
-  // ...vitalFooterBase.WithRewardsExpanding2XL,
+  ...vitalFooterBase.WithRewardsExpanding2XL,
   Components: {
     Rewards: flowHoc(
       on(RewardsClean)(listerineRewards.Default),
       withDesign({
-        Footnote: replaceWith(() => null),
+        Footnote: '2xl:hidden md:block hidden',
       }),
     ),
   },
-  // @sam your choice if you want to use this token OR jsut rewrite this for listerine
   Theme: {
-    // ...vitalFooterBase.WithRewardsExpanding2XL.Theme,
+    ...vitalFooterBase.Default.Theme,
     _: listerineColor.TextWhite,
-    Wrapper: 'nothing',
+    Wrapper: 'footer-wave',
     Column2Wrapper: flowHoc(
       as(listerineColor.BgSecondaryFooter),
-      addClasses('md:pl-10 mx-2'),
+      addClasses('md:pl-10 px-5 2xl:mt-[13vw] mobile-wave-bottom relative 2xl:w-8/12'),
     ),
-    Container: '2xl:m-0 2xl:pt-0 2xl:pb-0 mx-0',
-    // Rewards: flowHoc(
-    //   addClasses('py-20 px-10'),
-    //   withDesign({
-    //     Wrapper: as(listerineColor.BgSecondaryFooterSignUp),
-    //     Title: as(listerineTypography.H4),
-    //     Body: as(listerineTypography.Body),
-    //     FormWrapper: flowHoc(
-    //       addClasses('2xl:flex 2xl:flex-row 2xl:flex-wrap'),
-    //     ),
-    //     FormTextFirstName: flowHoc(
-    //       addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
-    //       as(listerineTypography.Body),
-    //     ),
-    //     FormTextEmail: flowHoc(
-    //       addClasses('md:rounded 2xl:bg-white 2xl:text-primary-body-copy 2xl:w-44 2xl:p-3'),
-    //       as(listerineTypography.Body),
-    //     ),
-    //     FormButton: flowHoc(
-    //       withParent(Div),
-    //       withDesign({
-    //         Parent: withPrependChild(
-    //           flowHoc(
-    //             addProps({
-    //               children:
-    //                  'The information you submit will be governed by our site’s PRIVACY POLICY'
-    //             }),
-    //             as(
-    //               listerineTypography.CrumbsReviews,
-    //               '2xl:text-white block 2xl:mb-3',
-    //             )
-    //           )(P), 'TEST'
-    //         ),
-    //       }),
-    //       addClasses('2xl:bg-interactive-primary md:p-4 2xl:rounded'),
-    //       as(
-    //         listerineTypography.Body,
-    //         vitalTextDecoration.Uppercase,
-    //       ),
-    //     ),
-    //   })
-    // ),
+    Container: flowHoc(
+      removeClasses('md:mx-4.5'),
+      addClasses('2xl:mx-0 2xl:px-0 2xl:pt-0 2xl:pb-0 mx-0'),
+    ),
   },
 });
 
