41c328919bc7b66fed713b9577170f95ce49a7bb
Samuel Davis <hello@smdvs.com>
2023-04-24 15:17:05 -0500

    Restore brand logo on header

--- packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
+++ packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
@@ -1,5 +1,8 @@
-import { replaceWith } from '@bodiless/fclasses';
+import {
+  Div, flowHoc, replaceWith
+} from '@bodiless/fclasses';
 import { asLogoToken, vitalLogoBase } from '@bodiless/vital-layout';
+import { withChild } from '@bodiless/core';
 import ListerineLogo from '../assets/ListerineLogo';
 
 const Default = asLogoToken({
@@ -7,7 +10,13 @@ const Default = asLogoToken({
   Layout: {
     ...vitalLogoBase.Default.Layout,
     Wrapper: 'w-full max-w-logo md:max-w-28 lg:min-w-28',
-    Image: replaceWith(ListerineLogo),
+    // Note: withChild does not work here, presumably because it's assumed to be adding
+    // a child element inside of img tags.
+    // Image: withChild(ListerineLogo),
+    Image: flowHoc(
+      replaceWith(Div),
+      withChild(ListerineLogo)
+    ),
   },
 });
 
