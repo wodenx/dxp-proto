eb05bdd8b72d3f062122b34e3d58ee4131a67a16
Samuel Davis <hello@smdvs.com>
2023-04-25 16:59:33 -0500

    Fix disappearing header logo

--- packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
+++ packages/__dxp__/src/components/Logo/tokens/listerineLogo.ts
@@ -2,7 +2,6 @@ import {
   Div, flowHoc, replaceWith
 } from '@bodiless/fclasses';
 import { asLogoToken, vitalLogoBase } from '@bodiless/vital-layout';
-import { withChild } from '@bodiless/core';
 import ListerineLogo from '../assets/ListerineLogo';
 
 const Default = asLogoToken({
@@ -15,7 +14,7 @@ const Default = asLogoToken({
     // Image: withChild(ListerineLogo),
     Image: flowHoc(
       replaceWith(Div),
-      withChild(ListerineLogo)
+      replaceWith(ListerineLogo),
     ),
   },
 });
