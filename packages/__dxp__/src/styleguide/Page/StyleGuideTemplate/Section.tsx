5f5d235c9d5933f71b4fca8cfb6c9ac633429fb4
Chris Oden <coden@its.jnj.com>
2023-04-26 14:15:16 -0400

    Editable sections

--- packages/__dxp__/src/styleguide/Page/StyleGuideTemplate/Section.tsx
+++ packages/__dxp__/src/styleguide/Page/StyleGuideTemplate/Section.tsx
@@ -6,13 +6,12 @@ import {
   H2,
   replaceWith,
   as,
-  addClasses,
-  addProps
 } from '@bodiless/fclasses';
 import { dxpProductSection } from '@kenvue/dxp-product';
 import { SectionClean, asSectionToken } from '@kenvue/dxp-section';
+import { withNode, withNodeKey } from '@bodiless/core';
 
-const WithSectionContentPocket = asSectionToken({
+const ProductCardSectionExample = asSectionToken({
   Spacing: {
     Wrapper: 'py-12',
   },
@@ -20,144 +19,31 @@ const WithSectionContentPocket = asSectionToken({
     Wrapper: 'border-b-4 border-slate-300',
   },
   Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-      addProps({
-        products: ['11gcuXew6vue7doHKlCE81', '6eXQKMmPXeOgBblBssQqdL'],
-      }),
-    ),
+    Content: 'flex',
   },
-  Content: {
-    Title: addProps({ children: 'Best Sellers (Two Products)'}),
+  Schema: {
+    _: withNode,
   },
 });
 
-const WithSectionContentZero = asSectionToken({
-  Spacing: {
-    Wrapper: 'py-12',
-  },
-  Theme: {
-    Wrapper: 'border-b-4 border-slate-300',
-  },
-  Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-      addProps({
-        products: ['5JLJSAJn9wuHnoWLyP3Lg5', '6fAXdpJM6ibblpTiO4JOqi', '4wjOoQHFSAqePnJMpNKEqg', '11gcuXew6vue7doHKlCE81'],
-      }),
-    ),
-  },
-  Content: {
-    Title: addProps({ children: 'Best Sellers (Four Products)'}),
-  },
-});
-
-const WithSectionContentCool = asSectionToken({
-  Spacing: {
-    Wrapper: 'py-12 border-b-4 border-slate-300',
-  },
-  Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-      addProps({
-        products: ['5JLJSAJn9wuHnoWLyP3Lg5', '6eXQKMmPXeOgBblBssQqdL', '6fAXdpJM6ibblpTiO4JOqi'],
-      }),
-    ),
-  },
-  Content: {
-    Title: addProps({ children: 'Best Sellers (Three Products: Cool Mint)'}),
-  },
-});
-
-const WithSectionContentCoolReordered = asSectionToken({
-  Spacing: {
-    Wrapper: 'py-12 border-slate-300',
-  },
-  Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-      addProps({
-        products: ['6fAXdpJM6ibblpTiO4JOqi', '5JLJSAJn9wuHnoWLyP3Lg5', '6eXQKMmPXeOgBblBssQqdL'],
-      }),
-    ),
-  },
-  Content: {
-    Title: addProps({ children: 'Best Sellers (Three Products: Cool Mint Reordered)'}),
-  },
-});
-
-const ProductCardSectionCool = as(
+const ProductExample = as(
   dxpProductSection.ProductCards,
-  WithSectionContentCool,
+  ProductCardSectionExample,
+  asSectionToken({
+    Schema: {
+      _: withNodeKey('product'),
+    }
+  }),
 )(SectionClean);
 
-const ProductCardSectionCoolReordered = as(
-  dxpProductSection.ProductCards,
-  WithSectionContentCoolReordered,
-)(SectionClean);
-
-const ProductCardSectionZero = as(
-  dxpProductSection.ProductCards,
-  WithSectionContentZero,
-)(SectionClean);
-
-const ProductCardSectionPocket = as(
-  dxpProductSection.ProductCards,
-  WithSectionContentPocket,
-)(SectionClean);
-
-const WithCollectionSelectedCollections = asSectionToken({
-  Spacing: {
-    Wrapper: 'py-12 border-b-4 border-slate-300',
-  },
-  Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-      addProps({
-        'product-collections': [
-          'concentrate',
-          'toothpaste',
-          'product-collection-w/o-image',
-          'mouthwash',
-          'on-the-go',
-          'concentrate',
-          'not-a-collection',
-          'mouthwash',
-          'ultraclean',
-          'mouthwash',
-          'on-the-go',
-          'antiseptic',
-        ],
-      }),
-    ),
-  },
-  Content: {
-    Title: addProps({ children: 'Selected Collections' }),
-  },
-});
-
-const WithCollectionDefault = asSectionToken({
-  Spacing: {
-    Wrapper: 'py-12 border-b-4 border-slate-300',
-  },
-  Layout: {
-    Content: flowHoc(
-      addClasses('flex'),
-    ),
-  },
-  Content: {
-    Title: addProps({ children: 'Default collection section'}),
-  },
-});
-
-const CollectionCardSectionSelected = as(
+const CollectionExample = as(
   dxpProductSection.ProductCollectionCards,
-  WithCollectionSelectedCollections,
-)(SectionClean);
-
-const CollectionCardSectionDefault = as(
-  dxpProductSection.ProductCollectionCards,
-  WithCollectionDefault,
+  ProductCardSectionExample,
+  asSectionToken({
+    Schema: {
+      _: withNodeKey('collection'),
+    }
+  }),
 )(SectionClean);
 
 const Subtitle = as(vitalTypography.H2, 'pt-8')(H2);
@@ -165,13 +51,9 @@ const Subtitle = as(vitalTypography.H2, 'pt-8')(H2);
 const Examples = () => (
   <>
     <Subtitle>Product Category Sections</Subtitle>
-    <CollectionCardSectionDefault />
-    <CollectionCardSectionSelected />
+    <CollectionExample />
     <Subtitle>Product Card Sections</Subtitle>
-    <ProductCardSectionPocket />
-    <ProductCardSectionZero />
-    <ProductCardSectionCool />
-    <ProductCardSectionCoolReordered />
+    <ProductExample />
   </>
 );
 
--- packages/dxp-product/src/components/ProductSection/tokens/dxpProductSection.ts
+++ packages/dxp-product/src/components/ProductSection/tokens/dxpProductSection.ts
@@ -2,21 +2,32 @@ import {
   flowHoc,
   replaceWith,
   on,
+  as,
+  H2,
+  TokenMeta,
 } from '@bodiless/fclasses';
 import {
   useNode,
   withNode,
   withDefaultContent,
+  ContentNode,
+  asReadOnly,
+  withContextActivator,
 } from '@bodiless/core';
 import { withPlaceholder } from '@bodiless/components';
 import { ListClean, asListToken, vitalList } from '@bodiless/vital-list';
 import { asSectionToken, dxpSection } from '@kenvue/dxp-section';
-import { vitalEditorPlain } from '@bodiless/vital-editors';
-import { CardStatic, vitalCardStatic } from '@bodiless/vital-card';
+import { vitalEditorPlain, EditorPlainClean } from '@bodiless/vital-editors';
+import { CardStatic, vitalCardStatic, CardClean } from '@bodiless/vital-card';
+import { withContentLibrary } from '@bodiless/layouts';
+import { ComponentSelector } from '@bodiless/layouts-ui';
 import type { DxpProductCardData, DxpProductCollectionCardData } from '../types';
+
+const LIBRARY_NODEKEY = '_library';
 
 const useCardData = () => {
   const { node } = useNode();
+
   return node.data;
 };
 const withCardData = withDefaultContent(useCardData);
@@ -26,28 +37,113 @@ const useProductCardContent = (props: any) => {
   // @todo: get rid of hardcoded node path and replace with page or site collection.
   const { data: allProducts } = node.peer<DxpProductCardData[]>('Products$allProducts');
   const { products: productIds } = props;
-  const listData = {
-    items: productIds,
-  };
   const cardData = allProducts.reduce((
     acc: { [key: string]: any },
     item: DxpProductCardData,
   ) => {
-    if (productIds.includes(item.id)) {
+    if (!productIds || productIds.includes(item.id)) {
       const { src, title, alt } = item.image;
       const content = {
         title: item.title,
         image: { src, title, alt },
       };
-      acc[item.id] = content;
+      acc[`${LIBRARY_NODEKEY}$${item.id}`] = content;
     }
     return acc;
   }, {});
   return {
-    '': listData,
     ...cardData,
   };
 };
+
+const parentNode = (node: ContentNode<any>) => {
+  const path = node.path.slice(0, -1);
+  return node.peer(path);
+};
+
+export const useLibraryNode = () => {
+  const { node } = useNode();
+  const libraryNode = parentNode(node).child(LIBRARY_NODEKEY);
+  return { node: libraryNode };
+};
+
+// We display nothing bc the title says it all
+const ProductDisplay = as(
+  vitalCardStatic.Product,
+  withCardData,
+)(CardClean);
+
+// export const useIsLinked = (): boolean => {
+//   const { node } = useNode<ArticleMetadata>();
+//   return !!node.data.revisionId;
+// };
+
+export const useProductLibraryOverrides = () => ({
+  name: 'product-library',
+  label: 'Select',
+  groupLabel: 'Product',
+  groupMerge: 'none' as 'none',
+  icon: 'web_stories',
+  // isHidden: useIsLinked(),
+  formTitle: 'Products',
+  formDescription: 'This is a list of all products on the site. Choose one to use it.',
+});
+
+export const useCollectionLibraryOverrides = () => ({
+  name: 'collection-library',
+  label: 'Select',
+  groupLabel: 'Collection',
+  groupMerge: 'none' as 'none',
+  icon: 'web_stories',
+  // isHidden: useIsLinked(),
+  formTitle: 'Product Collections',
+  formDescription: 'This is a list of all products collections on the site. Choose one to use it.',
+});
+
+const useProductLibraryMeta = (node: ContentNode<any>): TokenMeta => {
+  const { data } = node;
+  const { title } = data;
+  const { text } = title;
+  return {
+    title: text || 'No title',
+    categories: {
+      type: ['Product'],
+    },
+  };
+};
+
+const useCollectionLibraryMeta = (node: ContentNode<any>): TokenMeta => {
+  const { data } = node;
+  const { title } = data;
+  const { text } = title;
+  return {
+    title: text || 'No title',
+    categories: {
+      type: ['Product'],
+    },
+  };
+};
+
+const productLibraryOptions = {
+  DisplayComponent: ProductDisplay,
+  Selector: ComponentSelector,
+  useLibraryNode,
+  useMeta: useProductLibraryMeta,
+  useOverrides: useProductLibraryOverrides,
+  // peer: true,
+};
+
+const collectionLibraryOptions = {
+  DisplayComponent: ProductDisplay,
+  Selector: ComponentSelector,
+  useLibraryNode,
+  useMeta: useCollectionLibraryMeta,
+  useOverrides: useCollectionLibraryOverrides,
+  // peer: true,
+};
+
+const withProductLibrary = withContentLibrary(productLibraryOptions);
+const withCollectionLibrary = withContentLibrary(collectionLibraryOptions);
 
 const useProductCollectionCardContent = (props: any) => {
   const { node } = useNode();
@@ -64,31 +160,31 @@ const useProductCollectionCardContent = (props: any) => {
       image: { src, title, alt },
     };
     if (!productCollectionIds || productCollectionIds.includes(item.id)) {
-      acc[item.id] = content;
+      acc[`${LIBRARY_NODEKEY}$${item.id}`] = content;
     }
     return acc;
   }, {});
-  let listData = {};
-  if (!productCollectionIds) {
-    const cardKeys = Object.keys(cardData);
-    listData = {
-      items: cardKeys.filter(
-        //  dup check for collection card data.
-        (id: string, index: number) => (cardKeys.indexOf(id) === index),
-      )
-    };
-  } else {
-    listData = {
-      items: productCollectionIds.filter(
-        (id: string, index: number) => (
-          // existence and dup check for collection card data.
-          !!cardData[id] && (productCollectionIds.indexOf(id) === index)
-        ),
-      )
-    };
-  }
+  // let listData = {};
+  // if (!productCollectionIds) {
+  //   const cardKeys = Object.keys(cardData);
+  //   listData = {
+  //     items: cardKeys.filter(
+  //       //  dup check for collection card data.
+  //       (id: string, index: number) => (cardKeys.indexOf(id) === index),
+  //     )
+  //   };
+  // } else {
+  //   listData = {
+  //     items: productCollectionIds.filter(
+  //       (id: string, index: number) => (
+  //         // existence and dup check for collection card data.
+  //         !!cardData[id] && (productCollectionIds.indexOf(id) === index)
+  //       ),
+  //     )
+  //   };
+  // }
   return {
-    '': listData,
+    // '': listData,
     ...cardData,
   };
 };
@@ -109,7 +205,10 @@ const ProductCardList = asListToken({
     ...vitalList.Default.Components,
     Title: on(CardStatic)(
       vitalCardStatic.Product,
+      asReadOnly,
+      withContextActivator('onClick'),
       withCardData,
+      withProductLibrary,
     ),
   },
   Schema: {
@@ -127,6 +226,7 @@ const ProductCollectionList = asListToken({
     Title: on(CardStatic)(
       vitalCardStatic.Product,
       withCardData,
+      withCollectionLibrary,
     ),
   },
   Schema: {
@@ -140,7 +240,8 @@ const ProductCollectionList = asListToken({
 const Default = asSectionToken({
   ...dxpSection.Default,
   Components: {
-    Title: vitalEditorPlain.Default,
+    TitleWrapper: replaceWith(H2),
+    Title: on(EditorPlainClean)(vitalEditorPlain.Default),
     Link: replaceWith(() => null),
     Description: replaceWith(() => null),
   },
